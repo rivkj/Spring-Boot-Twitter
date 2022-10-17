55b5e439947152cc6a29d4319487846c0b777b68
kimberlyrivas <rivaskj07@gmail.com>
2022-10-17 17:32:22 -0400

    Second commit

---
+++ TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/configuration/WebMvcConfiguration.java
@@ -0,0 +1,22 @@
+package com.tts.TechTalentTwitter.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        BCryptPasswordEncoder bCryptPasswordEncoder = 
+//            new BCryptPasswordEncoder();
+//        return bCryptPasswordEncoder;
+//    }
+	@Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = 
+            new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+}
